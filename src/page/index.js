5b6f9d573baabb95340a37ead9d6b1dc4f3e014d
marcosdid <limafilhoguerreiro@outlook.com>
2020-06-20 23:31:46 -0300

    implementando os primeiros HTML e CSS

--- src/page/index.js
+++ src/page/index.js
@@ -1,43 +1,73 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+import api from '../api'
+
 import './App.css';
 import logoImg from '../assents/Logo.png'
 
 function App() {
+  //const [ books, setBooks ] = useState([])
+
+  async function handleSearchBook() {
+    const response = await api.get('50 tons de cinza')
+
+    console.log(response)
+  }
+
+
   return (
-    <div id>
-      <div id="main">
-        <header id="header-page">
-          <img src={logoImg} alt="Logo livros" id="logo"/>
-          <p>Pesquisa seu livro em interesse</p>
-          <input type="text"/>
-        </header>
+    <div id="content">
+      <header id="header-page">
+        <img src={logoImg} alt="Logo livros" id="logo"/>
+        <p>Pesquisa seu livro em interesse</p>
+        <input type="text"/>
+        <button type="button" onClick={handleSearchBook} >Pesquisar</button>
+      </header>
 
-        <main id="content-books">
-          <div className="book">
+      <main id="main">
+        <ul id="list-main">
+          <li className="data-book">
             <h1>Lorem ipsum</h1>
             <h2>NOME LOREM IPSUM</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vel tempora commodi cum dicta voluptatibus voluptatem culpa vitae nisi illo facilis, suscipit assumenda error dolorem quos, eos </p>
-          </div>
+          </li>
 
-          <div className="book">
+          <li className="data-book">
+            <h1 className="title">Lorem ipsum</h1>
+            <h2>NOME LOREM IPSUM</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vel tempora commodi cum dicta voluptatibus voluptatem culpa vitae nisi illo facilis, suscipit assumenda error dolorem quos, eos </p>
+          </li>
+
+          <li className="data-book">
+            <h1 className="title">Lorem ipsum</h1>
+            <h2>NOME LOREM IPSUM</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vel tempora commodi cum dicta voluptatibus voluptatem culpa vitae nisi illo facilis, suscipit assumenda error dolorem quos, eos </p>
+          </li>
+
+          <li className="data-book">
+            <h1 className="title">Lorem ipsum</h1>
+            <h2>NOME LOREM IPSUM</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vel tempora commodi cum dicta voluptatibus voluptatem culpa vitae nisi illo facilis, suscipit assumenda error dolorem quos, eos </p>
+          </li>
+
+          <li className="data-book">
+            <h1 className="title">Lorem ipsum</h1>
+            <h2>NOME LOREM IPSUM</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vel tempora commodi cum dicta voluptatibus voluptatem culpa vitae nisi illo facilis, suscipit assumenda error dolorem quos, eos </p>
+          </li>
+
+          <li className="data-book">
             <h1>Lorem ipsum</h1>
             <h2>NOME LOREM IPSUM</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vel tempora commodi cum dicta voluptatibus voluptatem culpa vitae nisi illo facilis, suscipit assumenda error dolorem quos, eos </p>
-          </div>
+          </li>
+        </ul>
+      </main>
 
-          <div className="book">
-            <h1>Lorem ipsum</h1>
-            <h2>NOME LOREM IPSUM</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vel tempora commodi cum dicta voluptatibus voluptatem culpa vitae nisi illo facilis, suscipit assumenda error dolorem quos, eos </p>
-          </div>
-
-          <div className="book">
-            <h1>Lorem ipsum</h1>
-            <h2>NOME LOREM IPSUM</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vel tempora commodi cum dicta voluptatibus voluptatem culpa vitae nisi illo facilis, suscipit assumenda error dolorem quos, eos </p>
-          </div>
-        </main>
-      </div>
+      <footer id="footer-page">
+        <p>Marcos Alves</p>
+        <a href="https://github.com/marcosdid">Github</a>
+      </footer>
     </div>
   );
 }
